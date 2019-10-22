4fce5ad9810d0ce37e1d77c71477ae6de7f76f14
aucoeur <aucoeur.ngo@students.makeschool.com>
2019-10-21 19:16:07 -0700

    Clean up rearrange.py

--- Code/rearrange.py
+++ Code/rearrange.py
@@ -1,21 +1,15 @@
-from random import randint
+from random import choice
 import sys
 
 def rearrange():
-
     unshuffled = sys.argv[1:]
     scrambled = []
 
-    count = 0
-    while count < len(unshuffled):
-        rand_num = randint(1, len(unshuffled))
-        rand_word = sys.argv[rand_num]
-        if rand_word in scrambled:
-            rand_word = sys.argv[rand_num]
-        else:
-            scrambled.append(rand_word)
-            count += 1
-    
+    while len(scrambled) != len(unshuffled):
+        random_word = choice(unshuffled)
+        if random_word not in scrambled:
+            scrambled.append(random_word)
+               
     print(' '.join(scrambled))
 
 if __name__ == "__main__":
