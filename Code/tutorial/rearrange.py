7aaaf89c5ed300320bd1626b544b435c67823752
aucoeur <aucoeur.ngo@students.makeschool.com>
2019-10-21 21:23:08 -0700

    Add stretch - string & sentence reversals

--- Code/tutorial/rearrange.py
+++ Code/tutorial/rearrange.py
@@ -1,8 +1,8 @@
 from random import choice
 import sys
 
-def rearrange():
-    unshuffled = sys.argv[1:]
+def rearrange(words):
+    unshuffled = words
     scrambled = []
 
     while len(scrambled) != len(unshuffled):
@@ -10,7 +10,19 @@ def rearrange():
         if random_word not in scrambled:
             scrambled.append(random_word)
                
-    print(' '.join(scrambled))
+    return ' '.join(scrambled)
+
+def string_reverse(word):
+    joined_word = ''.join(word)
+    sliced_word = joined_word[::-1]
+    return sliced_word
+
+def sentence_reverse(sentence):
+    reversed_sentence = string_reverse(sentence)
+    return ' '.join(reversed_sentence)
 
 if __name__ == "__main__":
-    rearrange()
+    words = sys.argv[1:]
+    # print(rearrange(words))
+    # print(string_reverse(words))
+    print(sentence_reverse(words))
