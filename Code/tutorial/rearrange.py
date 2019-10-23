9201c2c112aae88abe97c2ce221bf116ff440d4d
aucoeur <aucoeur.ngo@students.makeschool.com>
2019-10-23 09:48:51 -0700

    Add fisher_yates shuffle

--- Code/tutorial/rearrange.py
+++ Code/tutorial/rearrange.py
@@ -21,8 +21,20 @@ def sentence_reverse(sentence):
     reversed_sentence = sentence[::-1]
     return ' '.join(reversed_sentence)
 
+def fisher_yates(words):
+    for word in words:
+        random_word = choice(words)
+
+        x = words.index(word)
+        y = words.index(random_word)
+
+        words[x], words[y] = words[y], words[x]
+    
+    return ' '.join(words)
+
 if __name__ == "__main__":
     words = sys.argv[1:]
     # print(rearrange(words))
     # print(string_reverse(words))
-    print(sentence_reverse(words))
+    # print(sentence_reverse(words))
+    print(fisher_yates(words))
